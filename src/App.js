5c3d98d0876d29113597f80aea15ce97bb7216e9
Daniel Kai <danielkai@DanieldeMacBook-Air.local>
2023-05-07 20:29:02 +0800

    4

--- src/App.js
+++ src/App.js
@@ -6,61 +6,67 @@ import "./App.scss";
 
 function App() {
   const [selectedProducts, setSelectedProducts] = useState([]);
+  const toggleProduct = (selectedProduct) => {
+    setSelectedProducts((prevState) => {
+      const existingProductIndex = prevState.findIndex(
+        (product) => product.id === selectedProduct.id
+      );
 
-const toggleProduct = (selectedProduct) => {
-  setSelectedProducts((prevState) => {
-    const existingProductIndex = prevState.findIndex((product) => product.id === selectedProduct.id);
-
-    if (existingProductIndex > -1) {
-      // Update the existing product in the array
-      const updatedProducts = [...prevState];
-      updatedProducts[existingProductIndex] = selectedProduct;
-      return updatedProducts;
-    } else {
-      // Add the new product to the array
-      return [...prevState, selectedProduct];
-    }
-  });
-};
-
-
-const totalPrice = selectedProducts.reduce(
-  (acc, product) => acc + products.find((p) => p.id === product.id).price,
-  0
-);
-
+      if (existingProductIndex > -1) {
+        // Check if the selected variant is the same as the existing one
+        if (
+          prevState[existingProductIndex].selectedVariantIndex ===
+          selectedProduct.selectedVariantIndex
+        ) {
+          // Remove the product from the array
+          return prevState.filter(
+            (product) => product.id !== selectedProduct.id
+          );
+        } else {
+          // Update the existing product in the array
+          const updatedProducts = [...prevState];
+          updatedProducts[existingProductIndex] = selectedProduct;
+          return updatedProducts;
+        }
+      } else {
+        // Add the new product to the array
+        return [...prevState, selectedProduct];
+      }
+    });
+  };
+  const totalPrice = selectedProducts.reduce(
+    (acc, product) => acc + products.find((p) => p.id === product.id).price,
+    0
+  );
   const backgrounds = products.filter((product) => product.type === "background");
   const props = products.filter((product) => product.type === "prop");
+  const ProductLayer = ({ product }) => {
+    const [imageSrc, setImageSrc] = useState(null);
 
-const ProductLayer = ({ product }) => {
-  const [imageSrc, setImageSrc] = useState(null);
+    useEffect(() => {
+    const loadImage = async () => {
+      const imageModule = await product.imageModule();
+      setImageSrc(imageModule.default);
+    };
+    loadImage();
+  }, [product.imageModule, product]);
 
-  useEffect(() => {
-  const loadImage = async () => {
-    const imageModule = await product.imageModule();
-    setImageSrc(imageModule.default);
+
+    return (
+      <div className="product-layer">
+        {imageSrc && <img src={imageSrc} alt="selected product" className="product-image" />}
+      </div>
+    );
   };
-  loadImage();
-}, [product.imageModule, product]);
-
-
-  return (
-    <div className="product-layer">
-      {imageSrc && <img src={imageSrc} alt="selected product" className="product-image" />}
-    </div>
-  );
-};
-
-const updateSelectedVariantIndex = (productId, variantIndex) => {
-  setSelectedProducts(
-    selectedProducts.map((product) =>
-      product.id === productId
-        ? { ...product, selectedVariantIndex: variantIndex }
-        : product
-    )
-  );
-};
-
+  const updateSelectedVariantIndex = (productId, variantIndex) => {
+    setSelectedProducts(
+      selectedProducts.map((product) =>
+        product.id === productId
+          ? { ...product, selectedVariantIndex: variantIndex }
+          : product
+      )
+    );
+  };
 
   return (
     <article className="App">
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,58 +1,7 @@
 // ProductList.js
-import React, { useState, useEffect } from "react";
+import React from "react";
+import ProductCard from "./ProductCard";
 import "./ProductList.scss";
-
-const ProductCard = ({
-  product,
-  isSelected,
-  toggleProduct,
-  selectedVariantIndex,
-  updateSelectedVariantIndex,
-}) => {
-  const [imageSrc, setImageSrc] = useState(null);
-
-  const handleColorButtonClick = (index) => {
-    // Update the product color in the B section directly
-    if (isSelected) {
-      updateSelectedVariantIndex(product.id, index);
-    }
-  };
-
-  useEffect(() => {
-    const loadImage = async () => {
-      const imageModule = await product.variants[selectedVariantIndex].imageModule();
-      setImageSrc(imageModule.default);
-    };
-    loadImage();
-  }, [product, selectedVariantIndex]);
-
-  return (
-    <div
-      className={`product-card ${isSelected ? "selected" : ""}`}
-      onClick={() => toggleProduct(product, selectedVariantIndex)}
-    >
-      {imageSrc && <img src={imageSrc} alt="product" className="product-image" />}
-      <div>價格: {product.price}</div>
-      <div>尺寸: {product.size}</div>
-      {product.variants && (
-        <div className="color-selector">
-          {product.variants.map((variant, index) => (
-            <button
-              key={index}
-              className="color-option"
-              style={{ backgroundColor: variant.color }}
-              onClick={(e) => {
-                e.stopPropagation();
-                handleColorButtonClick(index);
-              }}
-            ></button>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
-
 
 
 const ProductList = ({ categoryName, products, selectedProducts, toggleProduct }) => {
