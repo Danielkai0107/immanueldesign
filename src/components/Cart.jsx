04214523c0a7b7869e9550aaa69e4dcbc32983ad
Daniel Kai <danielkai@DanieldeMacBook-Air.local>
2023-05-29 23:01:25 +0800

    2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,9 @@
 //Cart.js
 import axios from 'axios';
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Cart({selectedProducts,totalSelected,totalPrice}) {
-
-  const navigate = useNavigate();
 
   const [name, setName] = useState('');
   const [time, setTime] = useState('');
@@ -20,7 +18,7 @@ const handleNotify = async (event) => {
   event.preventDefault(); // 阻止表单的默认提交行为
   // 确保有选择产品
   if (selectedProducts.length === 0) {
-    setMessage('請選擇商品');
+    setMessage('尚未選擇商品');
     setTimeout(() => setMessage(''), 3000);
     return;
   }
@@ -53,47 +51,63 @@ const handleNotify = async (event) => {
 
     console.log(response.data); // 在控制台打印响应数据以供调试
 
-    setSuccess('發送成功');
-    setTimeout(() => setSuccess(''), 3000);
+    setSuccess('傳送成功\n請加入Line@與我們確認喔!');
+    setTimeout(() => setSuccess(''), 5000);
   } catch (error) {
     console.error(error); // 如果出现错误，将其打印到控制台
 
-    setError('發送失敗');
-    setTimeout(() => setError(''), 3000);
+    setError('不好意思，傳送有誤\n加入好友與我們洽談喔!');
+    setTimeout(() => setError(''), 5000);
   }
 };
+
+const handleCleanInput =()=>{
+  if (success !=='') {
+    setName('')
+    setTime('')
+    setPhone('')
+    setEmail('')
+  }
+}
 
   return (
     <article className='cart'>
       <ul className='btn'>
         <li>
-          <Link className='back-btn' onClick={() => navigate(-1)}></Link>
+          <Link className='back-btn' to='/Main'></Link>
+        </li>
+        <li>
+          {message !== '' && <p className='window-msg message'>{message}</p>}
+          {success !== '' && <p className='window-msg success'>
+            {success.split('\n').map((item, i) => { return <p key={i}>{item}</p>;})}</p>}
+          {error !== '' && <p className='window-msg error'>
+            {error.split('\n').map((item, i) => { return <p key={i}>{item}</p>;})}</p>}
         </li>
       </ul>
       <ul className='context'>
-        <li className='form'>
-          <p className='error'>{message}</p>
-          <p className='error'>{success}</p>
-          <p className='error'>{error}</p>
+        <p className='title'>【同在設計-預約單】</p>
+        <li className='info'>
+          <section className='number'>共計{totalSelected}項</section>
+          <section className='price'><span>佈置總金額：</span>＄{totalPrice}</section>
+        </li>
+        <li className='context-container'>
           <form onSubmit={handleNotify}>
             <label htmlFor="">姓名：<span>(必填)</span></label>
             <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder="" />
-            <label htmlFor="">手機號碼：<span>(必填)</span></label>
+            <label htmlFor="">手機號碼：<span>(訂單編號--必填)</span></label>
             <input type="text" value={phone} onChange={e => setPhone(e.target.value)} placeholder="" />
             <label htmlFor="">佈置日期：</label>
             <input type="date" value={time} onChange={e => setTime(e.target.value)} placeholder="" />
             <label htmlFor="">電子郵件：</label>
             <input type="text" value={email} onChange={e => setEmail(e.target.value)} placeholder="" />
-            <button type="submit">送出</button>
+            <button type="submit" onClick={handleCleanInput}>送出</button>
           </form>
-        </li>
-        <li className='info'>
-        <a href="https://lin.ee/8phpLYwB">
-          <img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="36" border="0"/>
-        </a>
-        <img src="https://qr-official.line.me/gs/M_802ygcsd_GW.png" alt="QRcode"/>
-        <section>{totalPrice}</section>
-        <section>{totalSelected}</section>
+          <section className='line'>
+            <a className='line-btn' href="https://lin.ee/8phpLYwB">
+              <img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友"/>
+            </a>
+            <img className='QRcode' src="https://qr-official.line.me/gs/M_802ygcsd_GW.png" alt="QRcode"/>
+          </section>
         </li>
       </ul>
     </article>
