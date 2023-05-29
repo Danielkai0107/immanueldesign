3ce1c8b7a8f6148be2382a194f4c49bf362e2800
Daniel Kai <danielkai@DanieldeMacBook-Air.local>
2023-05-29 23:44:42 +0800

    2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Home() {
+  const [current, setCurrent] = useState('p1');
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrent(prev => {
+        switch (prev) {
+          case 'p1': return 'p2';
+          case 'p2': return 'p3';
+          default: return 'p1';
+        }
+      });
+    }, 5000); // Change slide every 3 seconds
+
+    // Cleanup on unmount
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div>Home</div>
+    <ul className='home'>
+      <li className={current === 'p1' ? 'p1 active' : 'p1'}></li>
+      <li className={current === 'p2' ? 'p2 active' : 'p2'}></li>
+      <li className={current === 'p3' ? 'p3 active' : 'p3'}></li>
+    </ul>
   )
 }
 
